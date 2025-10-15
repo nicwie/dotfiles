0de245c4912ee5af02b60e7eb2832480eb51ae3b
nicwie <nic.wiesinger@yahoo.com>
2025-10-15 16:34:12 +0200

    Removed cache, etc from quickshell

--- dot_config/quickshell/basic/dot_qt/appuntitled_qml.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "appuntitled")
-set(working_dir "/home/nicwie1/.config/quickshell/basic")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/home/nicwie1/.config/quickshell/basic/.qt/appuntitled_qml.txt")
