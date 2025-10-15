f4b27688a1446ec7d130c4255fb8169d2be8f50b
nicwie <nic.wiesinger@yahoo.com>
2025-10-15 16:59:38 +0200

    Add omz custom folder

---
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,22 @@
+# Replaced with ~/.ssh/config, but keeping it here for posterity
+# alias ssh-lotte="ssh -p 2211 d300342@lotte.edvz.uni-linz.ac.at"
+alias ssh-lotte="ssh lotte"
+alias gh='history|grep'
+alias cd..='cd ..'
+alias wetter_alt="curl http://wttr.in/salzburg"
+alias wetter="curl v2d.wttr.in/Salzburg"
+alias resetkbd="sudo modprobe -r i8042 && sudo modprobe i8042"
+alias vi="nvim"
+alias vim="nvim"
+alias vimdiff="nvim -d"
+alias ls="exa"
+
+alias :q="exit"
+alias lsa='exa -lah --icons'
+alias l='exa -lah --icons'
+alias ls='exa --icons'
+alias ll='exa -lh --icons'
+alias la='exa -lah --icons'
+
+# For presentations, we want to have a light background for projectors
+alias zathura-presentation='zathura --config-dir ~/.config/zathura-presentation/zathurarc.light'
---
+++ dot_oh-my-zsh/custom/plugins/conda-zsh-completion/README.rst
@@ -0,0 +1,9 @@
+conda-zsh-completion
+--------------------
+
+Please see the top of the ``_conda`` file for more info.
+
+:License: |license|
+
+` BSD-3-Clause <https://opensource.org/license/BSD-3-clause>`_
+
