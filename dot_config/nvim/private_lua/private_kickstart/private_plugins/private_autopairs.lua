2e2d29a7aaa66ad22be5a4782d5a8df017615dac
nicwie <nic.wiesinger@yahoo.com>
2025-10-16 01:22:11 +0200

    Disable autopairs in tex

--- dot_config/nvim/private_lua/private_kickstart/private_plugins/private_autopairs.lua
+++ dot_config/nvim/private_lua/private_kickstart/private_plugins/private_autopairs.lua
@@ -8,21 +8,9 @@ return {
     dependencies = { "hrsh7th/nvim-cmp" },
     config = function()
         local npairs = require("nvim-autopairs")
-        local rule = require("nvim-autopairs.rule")
 
-        npairs.setup({})
-
-        -- Checks if this is in a mathzone in latex
-        local function is_not_in_mathzone()
-            if vim.bo.filetype == "tex" then
-                return vim.fn["vimtex#syntax#in_mathzone"]() == 0
-            end
-            return true
-        end
-
-        npairs.add_rules({
-            rule("(", ")"):with_pair(is_not_in_mathzone),
-            rule("[", "]"):with_pair(is_not_in_mathzone),
+        npairs.setup({
+            disable_filetype = { "TelescopePrompt", "spectre_panel", "snacks_picker_input", "tex", "latex" },
         })
 
         -- If you want to automatically add `(` after selecting a function or method
